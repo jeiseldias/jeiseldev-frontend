b7fb4cc2d6673a8b63377aa5c20f0ee59654b3b2
Jeisel <jeisel@gmail.com>
2022-05-18 16:50:44 -0300

    Changing img to image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 import styles from '../styles/Home.module.css'
@@ -13,25 +14,25 @@ export default function Home() {
             </Head>
 
             <section className={styles.Logo}>
-                <img src='./logo.png' alt='Logo jeisel.dev' />
+                <Image src='/logo.png' alt='Logo jeisel.dev' width={125} height={125} />
             </section>
 
             <section className={styles.SocialMedia}>
                 <ul>
                     <li>
-                        <a href='https://www.linkedin.com/in/jeisel/' target='_blank'>
+                        <a href='https://www.linkedin.com/in/jeisel/' target='_blank' rel='noopener'>
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
                     </li>
 
                     <li>
-                        <a href='https://github.com/jeiseldias' target='_blank'>
+                        <a href='https://github.com/jeiseldias' target='_blank' rel='noopener'>
                             <FontAwesomeIcon icon={faGithub} />
                         </a>
                     </li>
 
                     <li>
-                        <a href='https://www.instagram.com/jeiselsilva/' target='_blank'>
+                        <a href='https://www.instagram.com/jeiselsilva/' target='_blank' rel='noopener'>
                             <FontAwesomeIcon icon={faInstagram} />
                         </a>
                     </li>
